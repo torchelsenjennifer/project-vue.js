97d0be995af0239036af9935bd6f1cb48f697690
Jennifer <jennifertorchelsen@gmail.com>
2024-01-21 14:43:14 -0300

    ⚡ ┇ single-file components(SFC)

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,90 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import Github from './components/Github.vue';
+export default {
+	components: {
+		Github
+	}
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<Github />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+	background-color: #1c1a1d;
+	color: #e5e5e5;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+#app {
+	font-family: 'Ubuntu', sans-serif;
+	min-height: 100vh;
+	max-width: 40rem;
+	margin: 0 auto;
+	text-align: center;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+img {
+	border: 1px solid #e5e5e5;
+	border-radius: 999px;
+	display: block;
+	margin: 1rem auto;
+	width: 8rem;
+	height: 8rem;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1,
+h2 {
+	color: #f64348;
+	margin: 1rem auto .25rem;
+}
+
+h3 {
+	margin: 1rem auto .25rem;
+}
+
+span {
+	display: block;
+	margin: 1rem auto;
+}
+
+input,
+button {
+	max-width: 20rem;
+	padding: .5rem;
+}
+
+input {
+	background-color: #1c1a1d;
+	border: 1px solid #f64348;
+	border-radius: .25rem;
+	color: #e5e5e5;
+	margin: 1rem 1rem 1rem 0;
+}
+
+button {
+	background-color: #f64348;
+	border: unset;
+	border-radius: .25rem;
+	color: #1c1a1d;
+	font-size: 1rem;
+	font-weight: 700;
+	text-transform: uppercase;
+}
+
+button:hover {
+	cursor: pointer;
+	filter: brightness(0.95);
+}
+
+a {
+	color: #f64348;
 }
 </style>
---
+++ src/components/Github.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+	data() {
+		return {
+			name: "Jennifer Torchelsen",
+			login: "torchelsenjennifer",
+			bio: "...",
+			company: "Acne Inc.",
+			avatar_url: "https://unsplash.it/256",
+			searchInput: "",
+		};
+	},
+	methods: {
+		async fetchGithubUser() {
+			const res = await fetch(
+				`https://api.github.com/users/${this.searchInput}`
+			);
+			const { login, name, bio, company, avatar_url } = await res.json();
+
+			this.login = login;
+			this.name = name;
+			this.bio = bio;
+			this.company = company;
+			this.avatar_url = avatar_url;
+		},
+	},
+}
+</script>
+
+<template>
+      <h1>GitHub User Data</h1>
+      <input type="text" v-model="searchInput" />
+      <button v-on:click="fetchGithubUser">Carregar Usuario</button>
+      <img v-bind:src="avatar_url" />
+      <strong>@{{ login }}</strong>
+      <h2>{{ name }}</h2>
+      <h3>{{ company }}</h3>
+      <span>{{ bio }}</span>
+</template>
