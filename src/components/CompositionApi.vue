822076a00e60dd8bd11c9c6b65fe10b8693e3aee
Jennifer <jennifertorchelsen@gmail.com>
2024-01-21 17:28:59 -0300

    🔨 ┇  Styling

--- src/components/CompositionApi.vue
+++ src/components/CompositionApi.vue
@@ -53,3 +53,62 @@ async function fetchUserRepositories(username) {
 		</article>
 	</section>
 </template>
+
+<style scoped>
+img {
+	border: 1px solid #e5e5e5;
+	border-radius: 999px;
+	display: block;
+	margin: 1rem auto;
+	width: 8rem;
+	height: 8rem;
+}
+
+h1,
+h2 {
+	color: #f64348;
+	margin: 1rem auto .25rem;
+}
+
+h3 {
+	margin: 1rem auto .25rem;
+}
+
+span {
+	display: block;
+	margin: 1rem auto;
+}
+
+input,
+button {
+	max-width: 20rem;
+	padding: .5rem;
+}
+
+input {
+	background-color: #1c1a1d;
+	border: 1px solid #f64348;
+	border-radius: .25rem;
+	color: #e5e5e5;
+	margin: 1rem 1rem 1rem 0;
+}
+
+button {
+	background-color: #f64348;
+	border: unset;
+	border-radius: .25rem;
+	color: #1c1a1d;
+	font-size: 1rem;
+	font-weight: 700;
+	text-transform: uppercase;
+}
+
+button:hover {
+	cursor: pointer;
+	filter: brightness(0.95);
+}
+
+a {
+	color: #f64348;
+}
+</style>
